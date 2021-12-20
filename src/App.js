950f7d66bbb8f10a0e4fb644adc4fb882026b7ff
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-20 03:17:56 -0500

    added header elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <header>
+        <h1>Memory Card</h1>
+        <p>
+          <span>score:</span>
+          <span>0</span>
+        </p>
+        <p>
+          <span>high score:</span>
+          <span>0</span>
+        </p>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
