ad9e8610bf0131b2ddbb77ad3e984323f83c0634
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-21 01:40:47 -0500

    added gameboard with Card components and click listener

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import Card from './components/Card';
 import './App.css';
 
 const App = () => {
+
+  const play = () => {
+    console.log('click');
+  };
+
   return (
     <div>
       <header>
         <h1>Memory Card</h1>
-        <p>
-          <span>score:</span>
-          <span>0</span>
-        </p>
-        <p>
-          <span>high score:</span>
-          <span>0</span>
-        </p>
+        <div className="scoreboard">
+          <p>
+            <span>score:</span>
+            <span className="scoreboard-num">0</span>
+          </p>
+          <p>
+            <span>high score:</span>
+            <span className="scoreboard-num">0</span>
+          </p>
+        </div>
       </header>
+      <div className="gameboard">
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+        <Card play={play} />
+      </div>
     </div>
   );
 };
