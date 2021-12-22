0255711968f82ab3d0b3eecc43d42a8f56befc80
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-21 21:10:34 -0500

    imported image assets and applied them to cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import Card from './components/Card';
 import './App.css';
+import ahri from './images/ahri-card.jpg';
+import evelynn from './images/evelynn-card.jpg';
+import fiora from './images/fiora-card.jpg';
+import irelia from './images/irelia-card.jpg';
+import missFortune from './images/missfortune-card.jpg';
+import pantheon from './images/pantheon-card.jpg';
+import samira from './images/samira-card.jpg';
+import sylas from './images/sylas-card.jpg';
+import syndra from './images/syndra-card.jpg';
+import twistedFate from './images/twistedfate-card.jpg';
+import volibear from './images/volibear-card.jpg';
+import xayah from './images/xayah-card.jpg';
+import yasuo from './images/yasuo-card.jpg';
+import yone from './images/yone-card.jpg';
 
 const App = () => {
 
@@ -24,15 +38,20 @@ const App = () => {
         </div>
       </header>
       <div className="gameboard">
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
-        <Card play={play} />
+        <Card play={play} image={ahri} />
+        <Card play={play} image={evelynn} />
+        <Card play={play} image={fiora} />
+        <Card play={play} image={irelia} />
+        <Card play={play} image={missFortune} />
+        <Card play={play} image={pantheon} />
+        <Card play={play} image={samira} />
+        <Card play={play} image={sylas} />
+        <Card play={play} image={syndra} />
+        <Card play={play} image={twistedFate} />
+        <Card play={play} image={volibear} />
+        <Card play={play} image={xayah} />
+        <Card play={play} image={yasuo} />
+        <Card play={play} image={yone} />
       </div>
     </div>
   );
