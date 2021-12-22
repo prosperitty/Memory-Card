cb7c0f25fc70701284538e6e0092242381dce438
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-21 21:11:52 -0500

    updated src attribute which receives a props image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ahri from '../images/AhriSquare.jpg';
 
 const Card = props => {
   return (
     <div className='img-container'>
-      <img src={ahri} onClick={props.play}/>
+      <img src={props.image} onClick={props.play}/>
     </div>
   );
 };
